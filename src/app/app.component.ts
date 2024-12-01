465b36beb1d94aaac8a51b63be804882520be016
Matic Trebusak <matic.trebusak@gmail.com>
2024-12-01 21:49:03 +0100

    Added basic file reading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, effect, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<div>
+    @for (line of fileContentParsedByLine(); track $index) {
+    <div>{{ line }}</div>
+    }
+  </div>`,
+  imports: [],
 })
 export class AppComponent {
-  title = 'advent-of-code-2024';
+  httpClient = inject(HttpClient);
+  fileContent = toSignal(
+    this.httpClient.get('/assets/1-1-test.txt', { responseType: 'text' })
+  );
+  fileContentParsedByLine = computed(() => {
+    return this.fileContent()?.split('\n');
+  });
+
+  constructor() {
+    effect(() => {
+      console.log('fileContent', this.fileContent());
+    });
+  }
 }
