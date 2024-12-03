04fb72724dc8b19053ab9e97f92ed8866e5879b9
Matic Trebusak <matic.trebusak@razum.si>
2024-12-03 08:15:42 +0100

    Puzzle 3-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,16 +24,43 @@ export class AppComponent {
 
   sumOfMultiplications = computed(() => {
     let textInput = this.parsedInput()!;
+    let doesWork = this.prepareDoesItWork(textInput);
+    let total = this.extractAndMultiply(textInput, doesWork);
+    return total;
+  });
+
+  prepareDoesItWork(input: string): Array<boolean> {
+    if (!input) return [];
+    let regexDo = new RegExp('do\\(\\)', 'g');
+    let regexDont = new RegExp("don't\\(\\)", 'g');
+    let doInputs = [...input.matchAll(regexDo)].map((item) => item.index);
+    let dontInputs = [...input.matchAll(regexDont)].map((item) => item.index);
+    let isWorking = true;
+
+    return [...input].map((_, index) => {
+      if (doInputs.includes(index)) {
+        isWorking = true;
+      } else if (dontInputs.includes(index)) {
+        isWorking = false;
+      }
+      return isWorking;
+    });
+  }
+
+  extractAndMultiply(input: string, doesItWork: Array<boolean>): number {
     let regex = new RegExp('mul\\((\\d{1,3}),(\\d{1,3})\\)', 'g');
     let total = 0;
     let result;
-    while ((result = regex.exec(textInput))) {
-      let [first, second] = result[0]
-        .replace('mul(', '')
-        .replace(')', '')
-        .split(',');
-      total += Number(first) * Number(second);
+
+    while ((result = regex.exec(input))) {
+      if (doesItWork[result.index]) {
+        let [first, second] = result[0]
+          .replace('mul(', '')
+          .replace(')', '')
+          .split(',');
+        total += Number(first) * Number(second);
+      }
     }
     return total;
-  });
+  }
 }
