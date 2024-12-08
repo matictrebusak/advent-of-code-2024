d7ce101dbdbe1edcf5d9f60f4e3cc534dc715eb9
Matic Trebusak <matic.trebusak@razum.si>
2024-12-08 21:40:25 +0100

    Puzzle 8-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, computed, inject, Signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
-
-enum Direction {
-  UP = 0,
-  RIGHT = 1,
-  DOWN = 2,
-  LEFT = 3,
-}
 
 interface Coordinate {
   X: number;
   Y: number;
-}
-
-enum PositionValue {
-  NORMAL,
-  OBSTACLE,
-  OUTSIDE,
 }
 
 @Component({
@@ -26,7 +13,9 @@ enum PositionValue {
   selector: 'app-root',
   template: `<div>
     <div>
-      {{ 'Part 1 - Path length: ' + computePath() }}
+      {{
+        'Part 1 - Number of unique antinode locations: ' + calculateAntinodes()
+      }}
     </div>
   </div>`,
   imports: [],
@@ -34,12 +23,11 @@ enum PositionValue {
 export class AppComponent {
   httpClient = inject(HttpClient);
 
-  direction = Direction.UP;
+  mapOfAntennas = new Map<string, Array<Coordinate>>();
+  mapOfAntinodes = new Map<string, number>();
 
-  path = new Map<string, number>();
-
-  // DATA_FILE_PATH = '/assets/6-1-test.txt';
-  DATA_FILE_PATH = '/assets/6-1.txt';
+  // DATA_FILE_PATH = '/assets/8-1-test.txt';
+  DATA_FILE_PATH = '/assets/8-1.txt';
 
   lines = toSignal(
     this.httpClient
@@ -47,98 +35,113 @@ export class AppComponent {
       .pipe(map((input) => input.split('\n')))
   );
 
-  grid: Signal<Array<Array<string>>> = computed(() => {
-    if (!this.lines()) return [[]];
-    return this.readGrid(this.lines()!);
-  });
-
-  computePath = computed(() => {
-    if (this.grid().length > 1) {
-      let currentPosition: Coordinate | null = this.findStart();
-      this.setPath(currentPosition);
-      console.log('this.path', this.path.size);
-      console.log('currentPosition', currentPosition);
-      while (currentPosition) {
-        currentPosition = this.takeAStep(currentPosition);
-      }
-      console.log('this.path', this.path);
-      return this.path.size;
-    } else {
-      return 0;
-    }
-  });
-
-  readGrid(lines: Array<string>): Array<Array<string>> {
-    const grid = new Array<Array<string>>();
-    lines.forEach((line) => {
-      grid.push(line.split(''));
+  prepareGrid(lines: Array<string> | undefined): Array<Array<string>> {
+    if (!lines) return [[]];
+    let grid = Array<Array<string>>();
+    lines.map((line, row) => {
+      grid[row] = line!.split('');
+      grid[row].forEach((value, column) => {
+        this.addToMapOfAntenas(value, { X: column, Y: row });
+      });
     });
+    console.log('grid', grid);
+    console.log('mapOfAntennas', this.mapOfAntennas);
     return grid;
   }
 
-  findStart(): Coordinate {
-    let startIndex: Coordinate = { X: 0, Y: 0 };
-    this.grid().forEach((row, rowIndex) => {
-      let cellIndex = row.findIndex((cell) => cell === '^');
-      if (cellIndex !== -1) {
-        startIndex = { X: cellIndex, Y: rowIndex };
+  addToMapOfAntenas(value: string, position: Coordinate) {
+    if (value !== '.') {
+      if (this.mapOfAntennas.has(value)) {
+        const newArray: Array<Coordinate> = this.mapOfAntennas.get(value)!;
+        newArray.push(position);
+        this.mapOfAntennas.set(value, newArray);
+      } else {
+        this.mapOfAntennas.set(value, [position]);
       }
+    }
+  }
+
+  calculateAntinodes = computed(() => {
+    const dataGrid = this.prepareGrid(this.lines());
+    this.mapOfAntennas.forEach((antennas) => {
+      this.checkNumberOfAntinodes(dataGrid, antennas);
     });
-    console.log('startIndex', startIndex);
-    return startIndex;
-  }
+    return this.mapOfAntinodes.size;
+  });
 
-  takeATurn() {
-    this.direction = (this.direction + 1) % 4;
-  }
-
-  takeAStep(currentPosition: Coordinate): Coordinate | null {
-    let stepCandidate: Coordinate;
-    switch (this.direction) {
-      case Direction.UP:
-        stepCandidate = { X: currentPosition.X, Y: currentPosition.Y - 1 };
-        break;
-      case Direction.RIGHT:
-        stepCandidate = { X: currentPosition.X + 1, Y: currentPosition.Y };
-        break;
-      case Direction.DOWN:
-        stepCandidate = { X: currentPosition.X, Y: currentPosition.Y + 1 };
-        break;
-      case Direction.LEFT:
-        stepCandidate = { X: currentPosition.X - 1, Y: currentPosition.Y };
-        break;
+  checkNumberOfAntinodes(
+    dataGrid: Array<Array<string>>,
+    antennas: Array<Coordinate>
+  ): void {
+    for (let i = 0; i < antennas.length - 1; i++) {
+      for (let j = i + 1; j < antennas.length; j++) {
+        console.log('antennas[i], antennas[i + 1]', antennas[i], antennas[j]);
+        this.setAntinodesForPair(dataGrid, antennas[i], antennas[j]);
+      }
     }
-
-    const candidatePositionValue = this.getPositionValue(stepCandidate);
-    if (candidatePositionValue === PositionValue.NORMAL) {
-      currentPosition = stepCandidate;
-      this.setPath(currentPosition);
-      console.log('currentPosition', currentPosition);
-    } else if (candidatePositionValue === PositionValue.OBSTACLE) {
-      this.takeATurn();
-    } else if (candidatePositionValue === PositionValue.OUTSIDE) {
-      return null;
-    }
-    return currentPosition;
   }
 
-  getPositionValue(position: Coordinate): PositionValue {
+  setAntinodesForPair(
+    dataGrid: Array<Array<string>>,
+    antenna1: Coordinate,
+    antenna2: Coordinate
+  ): void {
+    // console.log('antenna1', antenna1);
+    // console.log('antenna2', antenna2);
+    const diffX = antenna2.X - antenna1.X;
+    const diffY = antenna2.Y - antenna1.Y;
+    const absDiffX = Math.abs(antenna2.X - antenna1.X);
+    const absDiffY = Math.abs(antenna2.Y - antenna1.Y);
+    // console.log('absDiffX', absDiffX);
+    // console.log('absDiffY', absDiffY);
+    let antinodeCoordinate1: Coordinate = { X: 0, Y: 0 };
+    let antinodeCoordinate2: Coordinate = { X: 0, Y: 0 };
+    const minX = Math.min(antenna1.X, antenna2.X);
+    const maxX = Math.max(antenna1.X, antenna2.X);
+    const minY = Math.min(antenna1.Y, antenna2.Y);
+    const maxY = Math.max(antenna1.Y, antenna2.Y);
+    // console.log('minX', minX);
+    // console.log('maxX', maxX);
+    // console.log('minY', minY);
+    // console.log('maxY', maxY);
+
     if (
-      position.X < 0 ||
-      position.Y < 0 ||
-      position.X >= this.grid()[0].length ||
-      position.Y >= this.grid().length
+      (antenna1.X >= antenna2.X && antenna1.Y >= antenna2.Y) ||
+      (antenna2.X >= antenna1.X && antenna2.Y >= antenna1.Y)
     ) {
-      return PositionValue.OUTSIDE;
-    } else if (this.grid()[position.Y][position.X] === '#') {
-      return PositionValue.OBSTACLE;
+      antinodeCoordinate1 = {
+        X: minX - absDiffX,
+        Y: minY - absDiffY,
+      };
+      antinodeCoordinate2 = {
+        X: maxX + absDiffX,
+        Y: maxY + absDiffY,
+      };
     } else {
-      return PositionValue.NORMAL;
+      antinodeCoordinate1 = {
+        X: minX - absDiffX,
+        Y: maxY + absDiffY,
+      };
+      // console.log('prva ', antinodeCoordinate1);
+      antinodeCoordinate2 = {
+        X: maxX + absDiffX,
+        Y: minY - absDiffY,
+      };
+      // console.log('druga ', antinodeCoordinate2);
     }
+
+    this.writeAntinodeToMap(antinodeCoordinate1, dataGrid);
+    this.writeAntinodeToMap(antinodeCoordinate2, dataGrid);
   }
 
-  setPath(currentPosition: Coordinate) {
-    const entry = `x:${currentPosition.X}, y:${currentPosition.Y}`;
-    this.path.set(entry, 1);
+  writeAntinodeToMap(antinode: Coordinate, data: Array<Array<string>>): void {
+    const x = antinode.X;
+    const y = antinode.Y;
+    if (x >= 0 && y >= 0 && y < data.length && x < data?.[0].length) {
+      // console.log('correct antinode', antinode);
+      this.mapOfAntinodes.set(`${x},${y}`, 1);
+    } else {
+      console.log('wrong antinode', antinode);
+    }
   }
 }
---
+++ src/app/solutions/6.ts
@@ -0,0 +1,156 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+enum Direction {
+  UP = 0,
+  RIGHT = 1,
+  DOWN = 2,
+  LEFT = 3,
+}
+
+interface Coordinate {
+  X: number;
+  Y: number;
+}
+
+enum PositionValue {
+  NORMAL,
+  OBSTACLE,
+  OUTSIDE,
+}
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `<div>
+    <div>
+      {{ 'Part 1 - Path length: ' + computePath() }}
+    </div>
+  </div>`,
+  imports: [],
+})
+export class AppComponent {
+  httpClient = inject(HttpClient);
+
+  direction = Direction.UP;
+
+  path = new Map<string, number>();
+  pathWithDirection = new Array<string>();
+
+  DATA_FILE_PATH = '/assets/6-1-test.txt';
+  // DATA_FILE_PATH = '/assets/6-1.txt';
+
+  lines = toSignal(
+    this.httpClient
+      .get(this.DATA_FILE_PATH, { responseType: 'text' })
+      .pipe(map((input) => input.split('\n')))
+  );
+
+  grid: Signal<Array<Array<string>>> = computed(() => {
+    if (!this.lines()) return [[]];
+    return this.readGrid(this.lines()!);
+  });
+
+  computePath = computed(() => {
+    if (this.grid().length > 1) {
+      let currentPosition: Coordinate | null = this.findStart();
+      this.setPath(currentPosition);
+      console.log('this.path', this.path.size);
+      console.log('currentPosition', currentPosition);
+      while (currentPosition) {
+        currentPosition = this.takeAStep(currentPosition);
+      }
+      console.log('this.path', this.path);
+      return this.path.size;
+    } else {
+      return 0;
+    }
+  });
+
+  readGrid(lines: Array<string>): Array<Array<string>> {
+    const grid = new Array<Array<string>>();
+    lines.forEach((line) => {
+      grid.push(line.split(''));
+    });
+    return grid;
+  }
+
+  findStart(): Coordinate {
+    let startIndex: Coordinate = { X: 0, Y: 0 };
+    this.grid().forEach((row, rowIndex) => {
+      let cellIndex = row.findIndex((cell) => cell === '^');
+      if (cellIndex !== -1) {
+        startIndex = { X: cellIndex, Y: rowIndex };
+      }
+    });
+    console.log('startIndex', startIndex);
+    return startIndex;
+  }
+
+  takeATurn() {
+    this.direction = (this.direction + 1) % 4;
+  }
+
+  takeAStep(currentPosition: Coordinate): Coordinate | null {
+    let stepCandidate: Coordinate;
+    switch (this.direction) {
+      case Direction.UP:
+        stepCandidate = { X: currentPosition.X, Y: currentPosition.Y - 1 };
+        break;
+      case Direction.RIGHT:
+        stepCandidate = { X: currentPosition.X + 1, Y: currentPosition.Y };
+        break;
+      case Direction.DOWN:
+        stepCandidate = { X: currentPosition.X, Y: currentPosition.Y + 1 };
+        break;
+      case Direction.LEFT:
+        stepCandidate = { X: currentPosition.X - 1, Y: currentPosition.Y };
+        break;
+    }
+
+    const candidatePositionValue = this.getPositionValue(stepCandidate);
+    if (candidatePositionValue === PositionValue.NORMAL) {
+      currentPosition = stepCandidate;
+      this.setPath(currentPosition);
+      console.log('currentPosition', currentPosition);
+    } else if (candidatePositionValue === PositionValue.OBSTACLE) {
+      this.takeATurn();
+    } else if (candidatePositionValue === PositionValue.OUTSIDE) {
+      return null;
+    }
+    return currentPosition;
+  }
+
+  getPositionValue(position: Coordinate): PositionValue {
+    if (
+      position.X < 0 ||
+      position.Y < 0 ||
+      position.X >= this.grid()[0].length ||
+      position.Y >= this.grid().length
+    ) {
+      return PositionValue.OUTSIDE;
+    } else if (this.grid()[position.Y][position.X] === '#') {
+      return PositionValue.OBSTACLE;
+    } else {
+      return PositionValue.NORMAL;
+    }
+  }
+
+  setPath(currentPosition: Coordinate) {
+    const entry = `x:${currentPosition.X} y:${currentPosition.Y}`;
+    this.path.set(entry, 1);
+  }
+
+  checkPotentialObsticles() {
+    this.path.forEach((_value, key) => {
+      const coordinates = key.split(' ');
+      const x = parseInt(coordinates[0].split(':')[1]);
+      const y = parseInt(coordinates[1].split(':')[1]);
+      if (this.grid()[y][x] === '#') {
+        console.log('Obstacle found at x:', x, 'y:', y);
+      }
+    });
+  }
+}
